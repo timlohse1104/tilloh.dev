902bf816c83b248946d4eb92d7f160f3644c58ef
timlohse1104 <tim-lohse@gmx.de>
2024-01-14 15:47:26 +0100

    :tada: Added nx workspace with frontend / backend structure e and working sveltekit demo app

---
+++ frontend/README.md
@@ -0,0 +1,11 @@
+# frontend
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build frontend` to build the library.
+
+## Running unit tests
+
+Run `nx test frontend` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: './node_modules/.vite/.',
+
+  plugins: [nxViteTsPaths(), sveltekit()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    globals: true,
+    cache: { dir: './node_modules/.vitest' },
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: { reportsDirectory: './coverage/frontend', provider: 'v8' },
+  },
+});
