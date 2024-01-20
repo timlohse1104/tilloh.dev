c95e6e5f96242fb6443e8e70621d23a5cf934050
timlohse1104 <tim-lohse@gmx.de>
2024-01-20 23:00:45 +0100

    :fire: Removed .svelte-kit files from git index

--- frontend/.svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/catch-em-all/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/catch-em-all/+page.svelte";
--- frontend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.9mQooJ-8.js","_app/immutable/chunks/scheduler.7fJTJATM.js","_app/immutable/chunks/index.ESpeO4IN.js","_app/immutable/chunks/stores.besRvEek.js","_app/immutable/chunks/entry.M0uqKt0T.js","_app/immutable/chunks/index.UshG1r0L.js"];
-export const stylesheets = [];
-export const fonts = [];
