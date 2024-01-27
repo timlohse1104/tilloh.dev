46e080c8802c5c815cf1373ab594eb397fe3da6d
timlohse1104 <tim-lohse@gmx.de>
2024-01-27 16:09:26 +0100

    :sparkles: Added openapi

--- backend/apps/tilloh-dev/src/main.ts
+++ backend/apps/tilloh-dev/src/main.ts
@@ -2,6 +2,7 @@ import { ensureEnvValue } from '@backend/util';
 import { Logger, Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppController } from './controller/controller';
 
 @Module({
@@ -20,6 +21,16 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 61154;
+
+  const config = new DocumentBuilder()
+    .setTitle('tilloh.dev')
+    .setDescription('The official tilloh.dev API documentation 📖')
+    .setVersion('1.0')
+    .addTag('tilloh')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
