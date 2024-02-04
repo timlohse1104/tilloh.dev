9653954ae8faa370039397f29b34a9c3dca7ae62
timlohse1104 <tim-lohse@gmx.de>
2024-02-05 00:01:20 +0100

    :recycle: Refactored preset overlay in smui dialog

--- frontend/src/routes/memorandum/content/PresetOverlay.svelte
+++ frontend/src/routes/memorandum/content/PresetOverlay.svelte
@@ -3,6 +3,7 @@
   // @ts-nocheck
 
   import Accordion, { Content, Header, Panel } from '@smui-extra/accordion';
+  import Dialog, { Content as DialogContent, Title } from '@smui/dialog';
   import Fab, { Icon, Label } from '@smui/fab';
   import IconButton from '@smui/icon-button';
   import List, { Graphic, Item, Text } from '@smui/list';
@@ -67,43 +68,47 @@
   });
 </script>
 
-<section>
-  <button id="exitOverlay" on:click={closeOverlay}> X </button>
-
-  <div class="configPreview">
-    <h1>Einstellung der Link und Ordner Konfiguration</h1>
-
-    <p>
+<Dialog
+  bind:open={$presetOverlayOptions.showOverlay}
+  aria-labelledby="large-scroll-title"
+  aria-describedby="large-scroll-content"
+  surface$style="width: 50vw; max-width: calc(100vw - 32px);"
+>
+  <Title id="large-scroll-title">
+    Einstellung der Link und Ordner Konfiguration
+    <p class="large-scroll-subtitle">
       Sichere hier deine aktuellen Links und Ordner lokal als Datei, um sie auf
       einem beliebigen Gerät weiter zu verwenden.
     </p>
+  </Title>
 
-    <div class="buttons">
-      <input id="configInput" type="file" bind:files style="display: none" />
-      <Fab color="primary" on:click={triggerFileSelect} extended>
-        <Icon class="material-icons">file_upload</Icon>
-        <Label>Import</Label>
-      </Fab>
-      <Fab on:click={triggerFileDownload} extended>
-        <Icon class="material-icons">file_download</Icon>
-        <Label>Export</Label>
-      </Fab>
-    </div>
+  <div class="buttons">
+    <input id="configInput" type="file" bind:files style="display: none" />
+    <Fab color="primary" on:click={triggerFileSelect} extended>
+      <Icon class="material-icons">file_upload</Icon>
+      <Label>Import</Label>
+    </Fab>
+    <Fab on:click={triggerFileDownload} extended>
+      <Icon class="material-icons">file_download</Icon>
+      <Label>Export</Label>
+    </Fab>
+  </div>
 
-    <h2>Das ist deine aktuelle Konfiguration</h2>
+  <Title>Das ist deine aktuelle Konfiguration</Title>
 
-    <List dense>
-      <Item>
-        <Graphic class="material-icons">folder</Graphic>
-        <Text>{folderAmount} Ordner</Text>
-      </Item>
-      <Item>
-        <Graphic class="material-icons">link</Graphic>
-        <Text>{linkAmount} Links</Text>
-      </Item>
-    </List>
+  <List dense>
+    <Item>
+      <Graphic class="material-icons">folder</Graphic>
+      <Text>{folderAmount} Ordner</Text>
+    </Item>
+    <Item>
+      <Graphic class="material-icons">link</Graphic>
+      <Text>{linkAmount} Links</Text>
+    </Item>
+  </List>
 
-    <div class="accordion-container">
+  <DialogContent id="large-scroll-content">
+    <div>
       <Accordion>
         <Panel bind:open={detailOpen}>
           <Header
@@ -123,22 +128,22 @@
         </Panel>
       </Accordion>
     </div>
-  </div>
 
-  <Snackbar bind:this={presetUploadSnackbar}>
-    <Label>Konfiguration erfolgreich importiert</Label>
-    <Actions>
-      <IconButton class="material-icons" title="Dismiss">close</IconButton>
-    </Actions>
-  </Snackbar>
+    <Snackbar bind:this={presetUploadSnackbar}>
+      <Label>Konfiguration erfolgreich importiert</Label>
+      <Actions>
+        <IconButton class="material-icons" title="Dismiss">close</IconButton>
+      </Actions>
+    </Snackbar>
 
-  <Snackbar bind:this={presetDownloadSnackbar}>
-    <Label>Download der Konfiguration gestartet.</Label>
-    <Actions>
-      <IconButton class="material-icons" title="Dismiss">close</IconButton>
-    </Actions>
-  </Snackbar>
-</section>
+    <Snackbar bind:this={presetDownloadSnackbar}>
+      <Label>Download der Konfiguration gestartet.</Label>
+      <Actions>
+        <IconButton class="material-icons" title="Dismiss">close</IconButton>
+      </Actions>
+    </Snackbar>
+  </DialogContent>
+</Dialog>
 
 <svelte:window
   on:keyup={(event) => (event.code === 'Escape' ? closeOverlay() : 'foo')}
@@ -147,59 +152,6 @@
 <style lang="scss">
   @import '$lib/styles/_variables.scss';
 
-  section {
-    position: fixed;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    background-color: $darkgrey90;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    flex-direction: column;
-    justify-items: center;
-    align-items: center;
-  }
-
-  #exitOverlay {
-    background-color: $red70;
-    padding: 12px 18px;
-    color: white;
-    font-size: 18px;
-    border: solid 0px;
-    box-shadow: $sharpen;
-    position: absolute;
-    top: 0;
-    right: 0;
-
-    &:hover {
-      background-color: $red;
-    }
-  }
-
-  .configPreview {
-    width: 90%;
-    height: 100%;
-    background-color: $darkgrey80;
-    padding: 20px;
-    overflow: auto;
-
-    h1 {
-      margin-top: 3rem;
-    }
-
-    p {
-      text-align: center;
-    }
-
-    h2 {
-      margin-top: 2rem;
-      margin-bottom: 1rem;
-    }
-  }
-
   .code-container {
     width: 100%;
     margin: 0;
@@ -207,15 +159,23 @@
     background-color: $trans;
 
     code {
-      font-size: 14px;
       line-height: 1.5;
-      font-family: 'Fira Code', monospace;
+      font-size: 12px;
+      font-family: var(--font-mono);
+      background-color: var(--pre-background);
+      border-radius: 3px;
+      padding: 0.5em;
+      overflow-x: auto;
+      color: var(--color-text);
     }
   }
 
   .buttons {
     display: flex;
     justify-content: space-evenly;
-    margin-top: 2rem;
+  }
+
+  .large-scroll-subtitle {
+    font-size: 1rem;
   }
 </style>
