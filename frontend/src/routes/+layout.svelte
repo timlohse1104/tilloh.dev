0aae33d2d9c721f1b3f97682863043cb42b9e774
timlohse1104 <tim-lohse@gmx.de>
2024-02-24 20:03:08 +0100

    :sparkles: Added route dependend favicons

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
+  import { page } from '$app/stores';
   import Header from './Header.svelte';
   import './styles.css';
 </script>
+
+<svelte:head>
+  {#if $page.url.pathname === '/'}
+    <link rel="icon" href="/favicons/home.favicon.svg" />
+  {:else if $page.url.pathname === '/memorandum'}
+    <link rel="icon" href="/favicons/memorandum.favicon.svg" />
+  {:else if $page.url.pathname === '/catch-em-all'}
+    <link rel="icon" href="/favicons/catch-em-all.favicon.svg" />
+  {:else if $page.url.pathname === '/uno-sort'}
+    <link rel="icon" href="/favicons/uno-sort.favicon.svg" />
+  {:else if $page.url.pathname === '/about'}
+    <link rel="icon" href="/favicons/about.favicon.svg" />
+  {:else if $page.url.pathname === '/settings'}
+    <link rel="icon" href="/favicons/settings.favicon.svg" />
+  {/if}
+</svelte:head>
 
 <div class="app">
   <Header />
