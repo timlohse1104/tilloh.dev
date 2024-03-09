7e8a112ac4e0d4165da8409598799fdebb68a91e
timlohse1104 <tim-lohse@gmx.de>
2024-03-09 02:12:32 +0100

    :sparkles: Imported example websocket service

---
+++ backend/libs/todo/src/lib/messages.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+  // In-memory database
+  messages: Message[] = [
+    { name: 'Tim', text: 'Moin', timestamp: new Date() },
+    {
+      name: 'Laura',
+      text: "Na, wie geht's?",
+      timestamp: new Date(Date.now() + 5000),
+    },
+  ];
+  clientToUser: Record<string, string> = {};
+
+  constructor(
+    @InjectPinoLogger(MessagesService.name)
+    private readonly logger: PinoLogger
+  ) {}
+
+  create(createMessageDto: CreateMessageDto) {
+    const { name } = createMessageDto;
+    this.logger.trace(`Adding new message from ${name} to database.`);
+    const newMessage = { ...createMessageDto, timestamp: new Date() };
+    this.messages.push(newMessage);
+    this.logger.trace(`Returning new message from ${name}.`);
+    return newMessage;
+  }
+
+  findAll() {
+    return this.messages;
+  }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} message`;
+  // }
+
+  // update(id: number, updateMessageDto: UpdateMessageDto) {
+  //   return `This action updates a #${id} message`;
+  // }
+
+  remove(id: number) {
+    return `This action removes a #${id} message`;
+  }
+
+  identify(name: string, clientId: string) {
+    // Use in-memory dictionary to store the reference for client and user
+    this.clientToUser[clientId] = name;
+    return Object.values(this.clientToUser);
+  }
+
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+}
