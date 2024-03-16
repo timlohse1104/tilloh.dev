4435921c5de5dbe8b5995a7918e41467b418c553
timlohse1104 <tim-lohse@gmx.de>
2024-03-16 20:29:58 +0100

    :sparkles: Added todo controller library

---
+++ backend/libs/todo/todo-controller/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'todo-todo-controller',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/todo/todo-controller',
+};
