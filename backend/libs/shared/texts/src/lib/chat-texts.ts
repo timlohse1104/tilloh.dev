b7d2b2c9f9988289383ab3336a9812d1f67d372c
timlohse1104 <tim-lohse@gmx.de>
2024-03-16 20:47:04 +0100

    :sparkles: Added chat texts

---
+++ backend/libs/shared/texts/src/lib/chat-texts.ts
@@ -0,0 +1,12 @@
+export class IdentifiersTexts {
+  static readonly ATTEMPT_FIND_ALL = 'Attempt to list all chats.';
+  static readonly ATTEMPT_FIND_ONE = 'Attempt to get an chat.';
+  static readonly ATTEMPT_CREATE = 'Attempt to create an chat.';
+  static readonly ATTEMPT_UPDATE = 'Attempt to update an chat.';
+  static readonly ATTEMPT_DELETE = 'Attempt to delete an chat.';
+  static readonly FOUND_ONE = 'MongoDb responded, found chat.';
+  static readonly CREATED_ONE = 'MongoDb responded, created chat.';
+  static readonly UPDATED_ONE = 'MongoDb responded, updated chat.';
+  static readonly DELETE_ONE = 'MongoDb responded, deleted chat.';
+  static readonly NOT_FOUND = 'Identifier not found.';
+}
