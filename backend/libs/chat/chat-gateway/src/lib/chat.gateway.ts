fe9083fe8fc5d7bf6c7b59ae714d46757f7d1cca
timlohse1104 <tim-lohse@gmx.de>
2024-05-15 00:37:05 +0200

    :fire: Removed websocket swagger decorators as its not supported currently

--- backend/libs/chat/chat-gateway/src/lib/chat.gateway.ts
+++ backend/libs/chat/chat-gateway/src/lib/chat.gateway.ts
@@ -3,13 +3,10 @@ import {
   CreateMessageDto,
   IdentifyMessageDto,
   JoinRoomDto,
-  Message,
-  MessageDto,
   TypingDto,
   UpdateMessageDto,
 } from '@backend/shared/types';
 import { Logger } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
 import {
   ConnectedSocket,
   MessageBody,
@@ -32,10 +29,6 @@ export class ChatGateway {
 
   constructor(private readonly messagesService: MessagesService) {}
 
-  @ApiOkResponse({
-    description: 'Messages creation request successfully returned.',
-    type: MessageDto,
-  })
   @SubscribeMessage('createMessage')
   async create(@MessageBody() createMessageDto: CreateMessageDto) {
     const { chatId, name, timestamp } = createMessageDto;
@@ -52,49 +45,30 @@ export class ChatGateway {
     return message;
   }
 
-  @ApiOkResponse({
-    description: 'List of messages request successfully returned.',
-    type: [Message],
-  })
   @SubscribeMessage('findAllMessages')
   findAll(@MessageBody() chatId: string) {
     this.logger.verbose(`Returning all messages from the database.`);
     return this.messagesService.findAll(chatId);
   }
 
-  @ApiOkResponse({
-    description: 'Find a message request successfully returned.',
-    type: Message,
-  })
   @SubscribeMessage('findOneMessage')
   findOne(@MessageBody() identifyMessageDto: IdentifyMessageDto) {
     const { chatId, id } = identifyMessageDto;
     return this.messagesService.findOne(chatId, id);
   }
 
-  @ApiOkResponse({
-    description: 'Message update request successfully returned.',
-    type: Message,
-  })
   @SubscribeMessage('updateMessage')
   update(@MessageBody() updateMessageDto: UpdateMessageDto) {
     const { chatId, id } = updateMessageDto;
     return this.messagesService.update(chatId, id, updateMessageDto);
   }
 
-  @ApiOkResponse({
-    description: 'Message remove request successfully returned.',
-    type: Message,
-  })
   @SubscribeMessage('removeMessage')
   remove(@MessageBody() identifyMessageDto: IdentifyMessageDto) {
     const { chatId, id } = identifyMessageDto;
     return this.messagesService.remove(chatId, id);
   }
 
-  @ApiOkResponse({
-    description: 'Chat join room request successfully returned.',
-  })
   @SubscribeMessage('join')
   joinRoom(
     @MessageBody() joinRoomDto: JoinRoomDto,
@@ -105,9 +79,6 @@ export class ChatGateway {
     return this.messagesService.identify(chatId, name, client.id);
   }
 
-  @ApiOkResponse({
-    description: 'Client typing request successfully returned.',
-  })
   @SubscribeMessage('typing')
   async typing(
     @MessageBody() typingDto: TypingDto,
