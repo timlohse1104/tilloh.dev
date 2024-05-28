a790a2884af98bc36d74747c5cc1c615b2e9bff8
timlohse1104 <tim-lohse@gmx.de>
2024-05-29 01:21:05 +0200

    :label: Added jokes types

---
+++ backend/libs/shared/types/src/lib/jokes.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
+
+export class JokeDto {
+  @ApiProperty({ description: 'Joke text' })
+  @IsNotEmpty()
+  text: string;
+
+  @ApiProperty({ description: 'Language abbreviation' })
+  @IsNotEmpty()
+  languague: string;
+}
+
+export class JokesDto {
+  @ApiProperty({ description: 'Jokes list' })
+  jokes: JokeDto[];
+}
+
+export class CategoryDto {
+  @ApiProperty({ description: 'Category name' })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: 'Language abbreviation' })
+  @IsNotEmpty()
+  languague: string;
+}
+
+export class CategoriesDto {
+  @ApiProperty({ description: 'Categories list' })
+  categories: CategoryDto[];
+}
