317dae39a8dacac195f126b6251c0427a852ab45
timlohse1104 <tim-lohse@gmx.de>
2024-06-12 00:00:34 +0200

    :sparkles: Added further identifiers api functions

--- frontend/src/lib/api/identifiers.api.ts
+++ frontend/src/lib/api/identifiers.api.ts
@@ -14,3 +14,39 @@ export async function getIdentifiers(token: string) {
     },
   }).then((res) => res.json());
 }
+
+export async function createIdentifier(token: string, name: string) {
+  return await fetch(`${apiURL}/identifiers`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name }),
+  }).then((res) => res.json());
+}
+
+export async function updateIdentifier(
+  token: string,
+  id: string,
+  name: string
+) {
+  return await fetch(`${apiURL}/identifiers/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name }),
+  }).then((res) => res.json());
+}
+
+export async function deleteIdentifier(token: string, id: string) {
+  return await fetch(`${apiURL}/identifiers/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.json());
+}
