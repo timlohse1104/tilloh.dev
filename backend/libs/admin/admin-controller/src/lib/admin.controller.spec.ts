cd221a8765019856036d9a6169211e36a2616e57
timlohse1104 <tim-lohse@gmx.de>
2024-07-15 01:22:07 +0200

    :sparkles: Executed nx repair and added admin controller unit test

---
+++ backend/libs/admin/admin-controller/src/lib/admin.controller.spec.ts
@@ -0,0 +1,42 @@
+import { AdminService } from '@backend/admin-provider';
+import { InputVerifyAdmin, OutputVerifyAdmin } from '@backend/shared-types';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AdminController } from './admin.controller';
+
+describe('AdminController', () => {
+  let controller: AdminController;
+  let adminService: AdminService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AdminController],
+      providers: [
+        {
+          provide: AdminService,
+          useValue: {
+            verifyAdmin: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AdminController>(AdminController);
+    adminService = module.get<AdminService>(AdminService);
+  });
+
+  describe('verfiyAdmin', () => {
+    it('should return the output of verifing an admin', async () => {
+      // Arrange
+      const inputVerifyAdmin: InputVerifyAdmin = { id: '1' };
+      const outputVerifyAdmin: OutputVerifyAdmin = { isAdmin: true };
+      jest
+        .spyOn(adminService, 'verifyAdmin')
+        .mockResolvedValue(outputVerifyAdmin);
+
+      // Act and Assert
+      await expect(controller.verifyAdmin(inputVerifyAdmin)).resolves.toEqual(
+        outputVerifyAdmin,
+      );
+    });
+  });
+});
