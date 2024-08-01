cb8c7781ff1dabe15d68b58e8f2cd7fe986fd2bc
timlohse1104 <tim-lohse@gmx.de>
2024-08-01 13:48:51 +0200

    :sparkles: :white_check_mark: Added default test scenario for jokes mongodb service

---
+++ backend/libs/jokes/jokes-persistence/src/lib/jokes-mongodb.service.spec.ts
@@ -0,0 +1,41 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { JokesMongoDbService } from './jokes-mongodb.service';
+import { Joke } from './schema/jokes.schema';
+
+describe('JokesMongoDbService', () => {
+  let service: JokesMongoDbService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getModelToken(Joke.name),
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            findOneAndUpdate: jest.fn(),
+            findOneAndDelete: jest.fn(),
+          },
+        },
+        JokesMongoDbService,
+      ],
+    }).compile();
+
+    service = module.get<JokesMongoDbService>(JokesMongoDbService);
+  });
+
+  it('Service should be defined.', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('test', () => {
+    it('should return true', async () => {
+      // arrange
+
+      // act & assert
+      expect(true).toBe(true);
+    });
+  });
+});
