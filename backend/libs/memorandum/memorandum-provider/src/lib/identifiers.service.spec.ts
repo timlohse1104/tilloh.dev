3bf1eea6e0fc5e303fed39df5f5dc3a64f5e699d
timlohse1104 <tim-lohse@gmx.de>
2024-08-01 14:09:30 +0200

    :sparkles: :white_check_mark: Added default test scenario for identifiers service

---
+++ backend/libs/memorandum/memorandum-provider/src/lib/identifiers.service.spec.ts
@@ -0,0 +1,40 @@
+import { IdentifiersMongoDbService } from '@backend/memorandum/memorandum-persistence';
+import { Test, TestingModule } from '@nestjs/testing';
+import { IdentifiersService } from './identifier.service';
+
+describe('IdentifiersService', () => {
+  let service: IdentifiersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: IdentifiersMongoDbService,
+          useValue: {
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            findOneAndUpdate: jest.fn(),
+            findOneAndDelete: jest.fn(),
+          },
+        },
+        IdentifiersService,
+      ],
+    }).compile();
+
+    service = module.get<IdentifiersService>(IdentifiersService);
+  });
+
+  it('Service should be defined.', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('test', () => {
+    it('should return true', async () => {
+      // arrange
+
+      // act & assert
+      expect(true).toBe(true);
+    });
+  });
+});
