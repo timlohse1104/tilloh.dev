9581a8641694bff37266c63403b8fa999c74b5d8
timlohse1104 <tim-lohse@gmx.de>
2024-08-01 13:53:36 +0200

    :sparkles: :white_check_mark: Added default test scenario for jokes service

---
+++ backend/libs/jokes/jokes-provider/src/lib/jokes.service.spec.ts
@@ -0,0 +1,50 @@
+import { JokesMongoDbService } from '@backend/jokes/jokes-persistence';
+import { HttpService } from '@nestjs/axios';
+import { Test, TestingModule } from '@nestjs/testing';
+import { JokesService } from './jokes.service';
+
+describe('JokesService', () => {
+  let service: JokesService;
+  let jokesMongoDbServiceMock: JokesMongoDbService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: JokesMongoDbService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+        JokesService,
+      ],
+    }).compile();
+
+    service = module.get<JokesService>(JokesService);
+    jokesMongoDbServiceMock =
+      module.get<JokesMongoDbService>(JokesMongoDbService);
+  });
+
+  it('Service should be defined.', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('test', () => {
+    it('should return true', async () => {
+      // arrange
+
+      // act & assert
+      expect(true).toBe(true);
+    });
+  });
+});
