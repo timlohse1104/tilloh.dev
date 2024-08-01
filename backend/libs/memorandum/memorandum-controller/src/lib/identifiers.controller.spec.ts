2d9f4c03ee8b2d3684571ce8ce51b120c71e1288
timlohse1104 <tim-lohse@gmx.de>
2024-08-01 13:57:47 +0200

    :sparkles: :white_check_mark: Added default test scenario for identifiers controller

---
+++ backend/libs/memorandum/memorandum-controller/src/lib/identifiers.controller.spec.ts
@@ -0,0 +1,42 @@
+import { IdentifiersService } from '@backend/memorandum/memorandum-provider';
+import { Test, TestingModule } from '@nestjs/testing';
+import { IdentifiersController } from './identifiers.controller';
+
+describe('IdentifiersController', () => {
+  let controller: IdentifiersController;
+  let identifiersServiceMock: IdentifiersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: IdentifiersService,
+          useValue: {
+            listIdentifiers: jest.fn(),
+            getIdentifier: jest.fn(),
+            createIdentifier: jest.fn(),
+            updateIdentifier: jest.fn(),
+            deleteIdentifier: jest.fn(),
+          },
+        },
+      ],
+      controllers: [IdentifiersController],
+    }).compile();
+
+    controller = module.get<IdentifiersController>(IdentifiersController);
+    identifiersServiceMock = module.get<IdentifiersService>(IdentifiersService);
+  });
+
+  it('Controller should be defined.', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('test', () => {
+    it('should return true', async () => {
+      // arrange
+
+      // act & assert
+      expect(true).toBe(true);
+    });
+  });
+});
