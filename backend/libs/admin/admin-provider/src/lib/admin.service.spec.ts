435438f140054c6f40025246ae47b3b7aa0cd6e4
timlohse1104 <tim-lohse@gmx.de>
2024-08-01 14:59:59 +0200

    :sparkles: :white_check_mark: Added unit tests for admin service

--- backend/libs/admin/admin-provider/src/lib/admin.service.spec.ts
+++ backend/libs/admin/admin-provider/src/lib/admin.service.spec.ts
@@ -4,6 +4,7 @@ import { AdminService } from './admin.service';
 
 describe('AdminService', () => {
   let service: AdminService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,18 +20,29 @@ describe('AdminService', () => {
     }).compile();
 
     service = module.get<AdminService>(AdminService);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
   it('Service should be defined.', () => {
     expect(service).toBeDefined();
   });
 
-  describe('test', () => {
-    it('should return true', async () => {
+  describe('verifyAdmin', () => {
+    it('should return true if admin id could be validated against admin identifier', async () => {
       // arrange
+      const inputId = 'correctId';
+      configService.get = jest.fn().mockReturnValue(inputId);
 
       // act & assert
-      expect(true).toBe(true);
+      expect(service.verifyAdmin(inputId)).toEqual({ isAdmin: true });
+    });
+    it('should return false if admin id could not be validated against admin identifier', async () => {
+      // arrange
+      const inputId = 'wrongId';
+      configService.get = jest.fn().mockReturnValue('correctId');
+
+      // act & assert
+      expect(service.verifyAdmin(inputId)).toEqual({ isAdmin: false });
     });
   });
 });
