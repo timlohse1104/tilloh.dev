d3550dbce67d1597065cd575f6dffc0b7e7457c7
timlohse1104 <tim-lohse@gmx.de>
2024-08-01 13:18:40 +0200

    :sparkles: :white_check_mark: Added default test scenario for messages service

---
+++ backend/libs/chat/chat-provider/src/lib/messages.service.spec.ts
@@ -0,0 +1,42 @@
+import { ChatMongoDbService } from '@backend/chat/chat-persistence';
+import { Test, TestingModule } from '@nestjs/testing';
+import { MessagesService } from './messages.service';
+
+describe('MessagesService', () => {
+  let service: MessagesService;
+  let chatMongoDbServiceMock: ChatMongoDbService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: ChatMongoDbService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        MessagesService,
+      ],
+    }).compile();
+
+    service = module.get<MessagesService>(MessagesService);
+    chatMongoDbServiceMock = module.get<ChatMongoDbService>(ChatMongoDbService);
+  });
+
+  it('Service should be defined.', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('test', () => {
+    it('should return true', async () => {
+      // arrange
+
+      // act & assert
+      expect(true).toBe(true);
+    });
+  });
+});
