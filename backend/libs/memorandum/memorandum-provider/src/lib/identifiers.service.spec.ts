010b3e190a7d3ac10ccc8bd41c0fdbbe895122e8
timlohse1104 <tim-lohse@gmx.de>
2024-08-17 21:52:39 +0200

    :sparkles: :white_check_mark: Added unit tests for identifiers service

--- backend/libs/memorandum/memorandum-provider/src/lib/identifiers.service.spec.ts
+++ backend/libs/memorandum/memorandum-provider/src/lib/identifiers.service.spec.ts
@@ -1,4 +1,5 @@
 import { IdentifiersMongoDbService } from '@backend/memorandum/memorandum-persistence';
+import { mockIdentifierDto } from '@backend/util';
 import { Test, TestingModule } from '@nestjs/testing';
 import { IdentifiersService } from './identifier.service';
 
@@ -37,12 +38,78 @@ describe('IdentifiersService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('test', () => {
-    it('should return true', async () => {
+  describe('listIdentifiers', () => {
+    it('should list all identifiers', async () => {
       // arrange
+      const expectedIdentifiers = [
+        mockIdentifierDto({ _id: '1', name: 'identifier1' }),
+        mockIdentifierDto({ _id: '2', name: 'identifier2' }),
+      ];
+      jest
+        .spyOn(service, 'listIdentifiers')
+        .mockResolvedValue(expectedIdentifiers);
 
-      // act & assert
-      expect(true).toBe(true);
+      // act
+      const identifiers = await service.listIdentifiers();
+
+      // assert
+      expect(identifiers).toEqual(expectedIdentifiers);
+    });
+  });
+
+  describe('getIdentifier', () => {
+    it('should get an identifier', async () => {
+      // arrange
+      const identifier = mockIdentifierDto({ _id: '1', name: 'identifier1' });
+      jest.spyOn(service, 'getIdentifier').mockResolvedValue(identifier);
+
+      // act
+      const result = await service.getIdentifier({ id: '1' });
+
+      // assert
+      expect(result).toEqual(identifier);
+    });
+  });
+
+  describe('createIdentifier', () => {
+    it('should create an identifier', async () => {
+      // arrange
+      const identifier = mockIdentifierDto({ _id: '1', name: 'identifier1' });
+      jest.spyOn(service, 'createIdentifier').mockResolvedValue(identifier);
+
+      // act
+      const result = await service.createIdentifier({ name: 'identifier1' });
+
+      // assert
+      expect(result).toEqual(identifier);
+    });
+  });
+
+  describe('updateIdentifier', () => {
+    it('should update an identifier', async () => {
+      // arrange
+      const identifier = mockIdentifierDto({ _id: '1', name: 'identifier1' });
+      jest.spyOn(service, 'updateIdentifier').mockResolvedValue(identifier);
+
+      // act
+      const result = await service.updateIdentifier('1', identifier);
+
+      // assert
+      expect(result).toEqual(identifier);
+    });
+  });
+
+  describe('deleteIdentifier', () => {
+    it('should delete an identifier', async () => {
+      // arrange
+      const identifier = mockIdentifierDto({ _id: '1', name: 'identifier1' });
+      jest.spyOn(service, 'deleteIdentifier').mockResolvedValue(identifier);
+
+      // act
+      const result = await service.deleteIdentifier('1');
+
+      // assert
+      expect(result).toEqual(identifier);
     });
   });
 });
