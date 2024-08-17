bbab6ebfc092eee62ee77f6ddaa707e5429e41a9
timlohse1104 <tim-lohse@gmx.de>
2024-08-18 00:54:42 +0200

    :green_heart: Updated backend workflow to audit, lint and test affected nx projects and increased deployment counter

--- backend/libs/shared/common/types/src/lib/admin.dto.ts
+++ backend/libs/shared/common/types/src/lib/admin.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
+// TODO: Deployment counter: 1
+
 export class InputVerifyAdmin {
   @ApiProperty()
   @IsString()
