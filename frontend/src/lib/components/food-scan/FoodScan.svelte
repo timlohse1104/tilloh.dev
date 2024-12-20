b74d704f4429a02d5228a4bd9298e33fd29df326
timlohse1104 <tim-lohse@gmx.de>
2024-12-20 22:45:58 +0100

    :rocket: Updated food scanner styling and default prompt, also implemented preview image

--- frontend/src/lib/components/food-scan/FoodScan.svelte
+++ frontend/src/lib/components/food-scan/FoodScan.svelte
@@ -9,13 +9,74 @@
   const { model_list: availableModels } = prebuiltAppConfig;
   let selectedModel = availableModels[4];
   let engine: webllm.MLCEngineInterface;
-  const defaultSystemPrompt =
-    'Du erhältst vom User einen Text, der per OCR aus einem Bild der Rückseite einer Nahrungsmittel-Verpackung ausgelesen wurde. Du antwortest darauf ausschließlich mit der Bezeichnung des Lebensmittels (z.B. Vollmilchschokolade) und ob das Nahrungsmittel vegan ist ("vegan" / "nicht vegan" ). Vegan bedeuetet, dass das Lebensmittel nur aus pflanzlichen Inhaltsstoffe besteht. Trenne die beiden Informationen durch ein Semikolon.';
+  const defaultSystemPrompt = `Du erhältst vom User einen Text mit Inhaltsstoffen eines Nahrungsmittels. Du antwortest darauf ausschließlich mit der Bezeichnung des Lebensmittels und ob es vegan ist "ja" (das Lebensmittel enthält nur pflanzliche Inhaltsstoffe) / "nein" (das Lebensmittel enthält zumindest ein offensichtliches Tierprodukt). Trenne die beiden Informationen durch ein Semikolon.
+
+    Beispiel:
+    Text vom User: "1000Va
+    oodess
+    Fromarsac, Marsac sur l'Isle,
+    24052 Périgueux, Cedex 9, France
+    Brotaufstrich auf Mandelbasis
+    Zutaten: Wasser, 16% MANDELPÜREE,
+    Rapsöl, Inulin, Kartoffelstärke, Kräuter der Provence,
+    Kräuter, Knoblauch, Zitronensaftkonzentrat, Speisesalz,
+    natürliche Aromen, Stickstoff zum Aufschlagen.
+    Kann Spuren von Sellerie und weiteren Schalenfrüchten enthalten.
+    Durchschnittliche
+    Nährwerte pro
+    Brennwert
+    100 g
+    Portion
+    30 g
+    939 kJ/
+    282 kJ/
+    227 kcal
+    68 kcal
+    Fett
+    19 g
+    5,7 g
+    davon gesättigte
+    Fettsäuren
+    1,7 g
+    0,5 g
+    Kohlenhydrate
+    7,4 g
+    3272770 015653
+    Nach dem Öffnen alsbald
+    verzehren. Bei +2°C bis +8°C
+    mindestens haltbar bis:
+    siehe Deckelrand
+    2,2 g
+    davon Zucker
+    1,1 g
+    ‹ 0,5 g
+    Ballaststoffe
+    4,0 g
+    1,2 g
+    Eiweiß
+    4,6 g
+    Salz
+    1,4 g
+    1,1g
+    0,33 g
+    Eine Packung entspricht ca. 4,5 Portionen
+    Ich liebe
+    Verbraucherservice:
+    Käse.com
+    D PF 1627, 65006 Wiesbaden
+    140ge •
+    info@bresso.de
+    info@bresso.at
+    A25417-03
+    ZHau"
+    Antwort: "Brotaufstrich auf Mandelbasis;ja"
+    `;
   let systemPromptText = defaultSystemPrompt;
   let userPromptText = '';
   let llmResults = [];
   let promptResStats: any = {};
   let inputFiles: File[] = [];
+  let imagePreviewSrc = '';
   let loadModelInfo = '';
   let debugInfoActive = false;
   let ocrResponseTime = 0;
@@ -30,7 +91,10 @@
   $: totalTokens = promptResStats?.total_tokens || '-';
   $: prefillTokensPerS = promptResStats?.extra?.prefill_tokens_per_s || '-';
   $: decodeTokensPerS = promptResStats?.extra?.decode_tokens_per_s || '-';
-  $: if (inputFiles?.length > 0) generateUserPrompt();
+  $: if (inputFiles?.length > 0) {
+    imagePreviewSrc = URL.createObjectURL(inputFiles[0]);
+    generateUserPrompt();
+  }
   $: if (userPromptText) promptLLM();
   $: if (selectedModel) reloadModel();
 
@@ -54,9 +118,6 @@
     } catch (error) {
       console.error('Failed to create MLCEngine:', error);
     }
-
-    const initProgress = engine.getInitProgressCallback();
-    console.log('initProgress', initProgress);
   }
 
   async function generateUserPrompt() {
@@ -102,6 +163,7 @@
     console.log(`LLM responded within ${llmResponseTime} seconds.`, {
       promptResponse,
     });
+    console.log(promptResponse.choices[0].message.content);
     llmResults = promptResponse.choices;
     promptResStats = promptResponse.usage;
 
@@ -131,95 +193,158 @@
 
 <section>
   {#if !engineReady}
-    <Card padded class="margin-bottom:1rem;">
-      <h2>⚠️ This application takes a while to load. Please be patient.</h2>
+    <Card padded class="warning_hint">
+      <h2>⚠️ This application takes a while to load! ⚠️</h2>
+      <p>Please be patient</p>
       <p>{loadModelInfo}</p>
     </Card>
   {:else}
     <h3>
-      Upload an image of the backside of a food item to get an estimation.
+      Let's scan the backside of a food item to get an estimation wether it is
+      vegan or not.
     </h3>
-    <span>
-      <input
-        type="file"
-        accept="image/png, image/jpeg, application/pdf"
-        bind:files={inputFiles}
-      />
+    <label for="file-upload" class="custom_file_label">Choose file</label>
+    <input
+      id="file-upload"
+      class="file_input"
+      type="file"
+      accept="image/png, image/jpeg"
+      bind:files={inputFiles}
+    />
 
+    {#if inputFiles?.length > 0}
+      <div class="initial_scan_output">
+        <img class="image_preview" src={imagePreviewSrc || ''} alt="Preview" />
+
+        <div class="prompt_output">
+          {#if loading && inputFiles.length != 0}
+            <p>⌛ Loading...</p>
+          {:else if !loading && llmResults.length != 0}
+            {#each llmResults as responseTexts, index (index)}
+              <h3>{responseTexts.message.content.split(';')[0]}</h3>
+              <p id="generate-label">
+                {#if responseTexts.message.content
+                  .split(';')[1]
+                  .toLowerCase()
+                  .includes('ja')}
+                  <span>✅ Vegan.</span>
+                {:else}
+                  <span>❌ Not vegan.</span>
+                {/if}
+              </p>
+            {/each}
+          {/if}
+        </div>
+      </div>
       <FormField>
         <Checkbox bind:checked={debugInfoActive} />
-        <p slot="label">More information</p>
+        <p slot="label">Technical information</p>
       </FormField>
-    </span>
-
-    {#if loading && inputFiles.length != 0}
-      <p>⌛ Loading...</p>
-    {:else if !loading && llmResults.length != 0}
-      <h3>Einschätzung</h3>
-      {#each llmResults as responseTexts, index (index)}
-        <p id="generate-label">
-          {#if responseTexts?.message?.content?.toLowerCase() === 'vegan'}
-            <span>✅</span>
-          {:else}
-            <span>❌</span>
-          {/if}
-          {@html responseTexts.message.content
-            .split(';')
-            .map((line) => `<p style="margin: 0; font-size: 14px;">${line}</p>`)
-            .join('')}
-        </p>
-      {/each}
     {/if}
 
     {#if debugInfoActive}
-      <h3>Debug Information</h3>
-      <p>Selected model: {selectedModel?.model_id}</p>
-      <Select
-        class="select-model"
-        bind:value={selectedModel}
-        label="Select Model"
-      >
-        {#each availableModels as model}
-          <Option value={model}
-            >{model?.model_id} ({model.vram_required_MB}MB VRAM / {model
-              ?.overrides?.context_window_size} window size)</Option
-          >
-        {/each}
-      </Select>
+      <div>
+        <h3>Debug Information</h3>
+        <p>Selected model: {selectedModel?.model_id}</p>
+        <Select
+          class="select_model"
+          bind:value={selectedModel}
+          label="Select Model"
+        >
+          {#each availableModels as model}
+            <Option value={model}
+              >{model?.model_id} ({model.vram_required_MB}MB VRAM / {model
+                ?.overrides?.context_window_size} window size)</Option
+            >
+          {/each}
+        </Select>
 
-      <p>System Prompt</p>
-      <Card padded>{systemPromptText}</Card>
+        <p>System Prompt</p>
+        <Card padded>{systemPromptText}</Card>
 
-      <p>User Prompt</p>
-      <Card padded>{userPromptText}</Card>
+        <p>User Prompt</p>
+        <Card padded>{userPromptText}</Card>
 
-      <p>Stats</p>
-      <table>
-        <tr>
-          <th>ocr time</th>
-          <th>llm time</th>
-          <th>completion_tokens</th>
-          <th>prompt_tokens</th>
-          <th>total_tokens</th>
-          <th>prefill_tokens_per_s</th>
-          <th>decode_tokens_per_s</th>
-        </tr>
-        <tr>
-          <td>{ocrResponseTimeText}</td>
-          <td>{llmResponseTimeText}</td>
-          <td>{completionTokens}</td>
-          <td>{promptTokens}</td>
-          <td>{totalTokens}</td>
-          <td>{prefillTokensPerS}</td>
-          <td>{decodeTokensPerS}</td>
-        </tr>
-      </table>
+        <p>Stats</p>
+        <table>
+          <tr>
+            <th>ocr time</th>
+            <th>llm time</th>
+            <th>completion_tokens</th>
+            <th>prompt_tokens</th>
+            <th>total_tokens</th>
+            <th>prefill_tokens_per_s</th>
+            <th>decode_tokens_per_s</th>
+          </tr>
+          <tr>
+            <td>{ocrResponseTimeText}</td>
+            <td>{llmResponseTimeText}</td>
+            <td>{completionTokens}</td>
+            <td>{promptTokens}</td>
+            <td>{totalTokens}</td>
+            <td>{prefillTokensPerS}</td>
+            <td>{decodeTokensPerS}</td>
+          </tr>
+        </table>
+      </div>
     {/if}
   {/if}
 </section>
 
 <style lang="scss">
-  :global(.select-model) {
+  section {
+    padding-top: 2rem;
+    display: flex;
+    gap: 2rem;
+    align-items: center;
+    flex-direction: column;
+    height: 80vh;
+    overflow: auto;
+  }
+
+  :global(.warning_hint) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .initial_scan_output {
+    display: flex;
+    flex-direction: row;
+    gap: 2rem;
+  }
+
+  .prompt_output {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 0.25rem 0.25rem 0 0;
+  }
+
+  h3 {
+    margin: 0;
+  }
+
+  .image_preview {
+    max-width: 33vw;
+  }
+
+  .file_input {
+    opacity: 0;
+    position: absolute;
+    z-index: -1;
+  }
+
+  .custom_file_label {
+    display: inline-block;
+    padding: 10px 15px;
+    background-color: #007bff;
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  :global(.select_model) {
     width: 50vw;
   }
 
