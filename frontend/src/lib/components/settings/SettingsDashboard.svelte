9af403898968b15ba602ce13ac16516853371cd8
timlohse1104 <tim-lohse@gmx.de>
2025-02-21 23:42:28 +0100

    :recycle: :recycle: Refactored theme and language switch to settings page

---
+++ frontend/src/lib/components/settings/SettingsDashboard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { t } from '$lib/util/translations';
+  import DashboardCard from '../admin/DashboardCard.svelte';
+  import LanguageSwitch from '../shared/LanguageSwitch.svelte';
+  import ThemeSwitch from '../shared/ThemeSwitch.svelte';
+</script>
+
+<section>
+  <DashboardCard
+    header={$t('page.settings.dashboard.theme.header')}
+    description={$t('page.settings.dashboard.theme.description')}
+  >
+    <ThemeSwitch />
+  </DashboardCard>
+  <DashboardCard
+    header={$t('page.settings.dashboard.language.header')}
+    description={$t('page.settings.dashboard.language.description')}
+  >
+    <LanguageSwitch />
+  </DashboardCard>
+</section>
+
+<style lang="scss">
+  section {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    max-width: 100%;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
