8eafb02c4e493ae6028fac16e07e02433e8bf5f2
timlohse1104 <tim-lohse@gmx.de>
2025-05-08 15:55:55 +0200

    :rocket: Replaced global search with carbon variant

--- frontend/src/lib/components/home/SearchBar.svelte
+++ frontend/src/lib/components/home/SearchBar.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
   import { initialized, t } from '$lib/util/translations';
-  import { Icon } from '@smui/common';
-  import Paper from '@smui/paper';
-  import { Input } from '@smui/textfield';
+  import { Form, Search } from 'carbon-components-svelte';
 
   let value = '';
 </script>
 
 {#if $initialized}
   <div class="search_container">
-    <form action="https://duckduckgo.com/">
-      <Paper class="solo_paper" elevation={6}>
-        <Icon class="material-icons">search</Icon>
-        <Input
-          style="width: 250em;"
-          bind:value
-          placeholder={$t('page.home.searchPlaceholder')}
-          class="solo_input"
-          type="text"
-          name="q"
-          autofocus
-        />
-      </Paper>
-    </form>
+    <Form
+      on:submit={(e) => {
+        e.preventDefault();
+        window.location.href = `https://duckduckgo.com/?q=${encodeURIComponent(value)}`;
+      }}
+      class="global_search_form"
+    >
+      <Search
+        placeholder={$t('page.home.searchPlaceholder')}
+        autofocus
+        bind:value
+      />
+    </Form>
   </div>
 {:else}
   <div class="search_container">Locale initializing...</div>
@@ -35,27 +32,9 @@
     align-items: center;
     padding: 36px 18px;
     margin-top: 1em;
-  }
-
-  * :global(.solo_paper) {
-    display: flex;
-    align-items: center;
-    flex-grow: 1;
     width: 80vw;
-    margin: 0 12px;
-    padding: 0 12px;
-    height: 48px;
   }
-  * :global(.solo_paper > *) {
-    display: inline-block;
-    margin: 0 12px;
-  }
-  * :global(.solo_input) {
-    flex-grow: 1;
-    color: var(--mdc-theme-on-surface, #fff);
-  }
-  * :global(.solo_input::placeholder) {
-    color: var(--mdc-theme-on-surface, #fff);
-    opacity: 0.6;
+  :global(.global_search_form) {
+    width: 100%;
   }
 </style>
